550d5ef966a5b18e7be1ba447ff647b16e6b22da
Nixon <lazaronixon@hotmail.com>
2022-03-21 16:27:38 -0300

    initial

---
+++ src/main/java/com/koombea/shopifyintegrator/ShopifyintegratorApplication.java
@@ -0,0 +1,13 @@
+package com.koombea.shopifyintegrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyintegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyintegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koombea/shopifyintegrator/ShopifyintegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koombea.shopifyintegrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyintegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
